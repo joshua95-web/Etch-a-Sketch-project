6c5106c6d0dfab9def58c0cfefc48fa958c59f3c
joshua95-web <joshcscurfield@gmail.com>
2023-05-02 13:00:44 +0100

    finished for loop and put makeRows function call below it

--- script.js
+++ script.js
@@ -5,5 +5,8 @@ function makeRows(rows, cols) {
     grid.style.setProperty("grid-cols", cols);
     for (c = 0; c < (rows * cols); c++) {
         let cell = document.createElement("div");
-    }
-}
+        grid.appendChild(cell).className = "grid-item"
+    };
+};
+
+makeRows(16,16);
