eebc3d2a259fead09e06c2cfaeb31b7365257c9a
joshua95-web <joshcscurfield@gmail.com>
2023-05-02 12:57:20 +0100

    created for loop to make the grid

--- script.js
+++ script.js
@@ -3,4 +3,7 @@ const grid = document.createElement("div");
 function makeRows(rows, cols) {
     grid.style.setProperty("grid-rows", rows);
     grid.style.setProperty("grid-cols", cols);
+    for (c = 0; c < (rows * cols); c++) {
+        let cell = document.createElement("div");
+    }
 }
