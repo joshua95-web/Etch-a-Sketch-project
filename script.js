733f8063f735d5c2ddee1bd3ae2c8da6797429a7
joshua95-web <joshcscurfield@gmail.com>
2023-05-02 12:39:58 +0100

    created 16 divs in DOM and a container div called grid

---
+++ script.js
@@ -0,0 +1,20 @@
+const grid = document.createElement("div");
+
+document.appendChild(grid);
+
+const gridCell1 = document.createElement("div");
+const gridCell2 = document.createElement("div");
+const gridCell3 = document.createElement("div");
+const gridCell4 = document.createElement("div");
+const gridCell5 = document.createElement("div");
+const gridCell6 = document.createElement("div");
+const gridCell7 = document.createElement("div");
+const gridCell8 = document.createElement("div");
+const gridCell9 = document.createElement("div");
+const gridCell10 = document.createElement("div");
+const gridCell11 = document.createElement("div");
+const gridCell12 = document.createElement("div");
+const gridCell13 = document.createElement("div");
+const gridCell14 = document.createElement("div");
+const gridCell15 = document.createElement("div");
+const gridCell16 = document.createElement("div");
