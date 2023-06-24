7582ebf94549a4be9ff2e9b6deddfc8be8c345d9
kali <kali@kali>
2023-06-24 15:25:51 +0100

    rebuilt makeRows() function, creating the grid as styling properties in the DOM

--- script.js
+++ script.js
@@ -1,5 +1,14 @@
-const grid = document.createElement("div");
+const gridContainer = document.createElement("div");
 
+function makeRows(rows, cols) {
+    gridContainer.style.setProperty('--grid-rows', rows);
+    gridContainer.style.setProperty('--grid-cols', cols);
+        for (c = 0; c < (rows * cols); c++) {
+            let cell = document.createElement("div");
+            gridContainer.appendChild(cell).className = "grid-item";        
+        };
+};
 
+makeRows(16, 16);
 
 grid.style.borderColor = "solid red";
