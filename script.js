68370ed3d6db5937aea7eb9987e475f124f544ca
kali <kali@kali>
2023-06-24 14:45:25 +0100

    removed makerows function to start it from scratch, for the sake of clarity

--- script.js
+++ script.js
@@ -1,14 +1,5 @@
 const grid = document.createElement("div");
 
-function makeRows(rows, cols) {
-    grid.style.setProperty("grid-rows", rows);
-    grid.style.setProperty("grid-cols", cols);
-    for (c = 0; c < (rows * cols); c++) {
-        let cell = document.createElement("div");
-        grid.appendChild(cell).className = "grid-item"
-    };
-};
 
-makeRows(16,16);
 
 grid.style.borderColor = "solid red";
